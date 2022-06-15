7f118df33b524ab8a32823ab8f3e7b3d02cb1b22
Michal Szabo <michal.szabo@email.cz>
2022-06-15 10:28:48 +0200

    be-api-secure: Allow CORS + secure API with helmet + use request speed limiter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,48 @@
 import path from "path";
 import express from "express";
-import type { Express } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import slowDown from "express-slow-down";
+
+import type { Express } from "express";
 
 import { notFound, errorHandler } from "./handlers";
 import connectDb from "./services/db.service";
 import CurrencyRoute from "./routes/currency";
 import StatsRoute from "./routes/stats";
 
-// Development env config
+/* Development env config */
 if (process.env.NODE_ENV === "development") {
   dotenv.config({
     path: path.join(__dirname, "..", "config", ".env.dev")
   });
 }
 
-// Express app instance
+/* Express app instance */
 const app: Express = express();
-
 app.use(express.json());
 
-// DB connector
+/* API secure */
+
+// Allow CORS
+app.use(cors());
+
+// Secure Express app by setting or removing some HTTP headers (X-Powered-By, ..)
+app.use(helmet());
+
+// Speed limiter
+const speedLimiter = slowDown({
+  windowMs: 60 * 1000, // 1 minute
+  delayAfter: 20, // Delay after 20 requests in 1 minute
+  delayMs: 500 // Every next request within that minute will take + 500 another ms
+});
+app.use(speedLimiter);
+
+/* DB connector */
 connectDb();
 
-// Routes
+/* Routes */
 app.use("/api/currency/", CurrencyRoute);
 app.use("/api/stats/", StatsRoute);
 
@@ -32,6 +51,7 @@ app.use(notFound);
 // Error handler
 app.use(errorHandler);
 
+/* Server start */
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
